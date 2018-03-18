34470f8fc7ed74abb1fe690db8625697b571b90a
CJMinecraft01 <speed2pc2@gmail.com>
2018-03-18 11:40:46 +0000

    Added item renders

--- src/main/java/cjminecraft/domesticcraft/utils/registries/Registerer.java
+++ src/main/java/cjminecraft/domesticcraft/utils/registries/Registerer.java
@@ -6,10 +6,14 @@ import cjminecraft.domesticcraft.init.DCItems;
 import cjminecraft.domesticcraft.utils.registries.annotations.RegisterBlock;
 import cjminecraft.domesticcraft.utils.registries.annotations.RegisterItem;
 import cjminecraft.domesticcraft.utils.registries.annotations.RegisterItemBlock;
+import cjminecraft.domesticcraft.utils.registries.annotations.RegisterRender;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -54,7 +58,7 @@ public class Registerer {
                     field.set(null, block);
                 }
 
-                ItemBlock item = null;
+                ItemBlock item;
                 if (!details.customItemBlock()) {
                     item = new ItemBlock(block);
                 } else if (block instanceof ICustomItemBlock) {
@@ -105,6 +109,53 @@ public class Registerer {
             }
         }
         DomesticCraft.LOGGER.info("Successfully registered " + registeredBlocks + " blocks!");
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        DomesticCraft.LOGGER.info("Searching for items to register renders for");
+        int registeredItems = 0;
+        for (Field field : DCItems.class.getDeclaredFields()) {
+            if (field.isAnnotationPresent(RegisterRender.class)) try {
+                RegisterRender details = field.getAnnotation(RegisterRender.class);
+                Item item = (Item) field.get(null);
+                if (item != null) {
+                    if (details.hasVariants())
+                        for (int meta = 0; meta < details.variants().length; meta++)
+                            ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(DomesticCraft.MODID, details.variants()[meta]), "inventory"));
+                    else
+                        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+                    registeredItems++;
+                } else {
+                    DomesticCraft.LOGGER.error("Unable to register renders for item: " + field.getName() + "! The item cannot be null!");
+                }
+            } catch (Exception e) {
+                DomesticCraft.LOGGER.error("Unable to register renders for item: " + field.getName() + "! The following error was thrown:");
+                DomesticCraft.LOGGER.catching(Level.ERROR, e);
+            }
+        }
+
+        for (Field field : DCBlocks.class.getDeclaredFields()) {
+            if (field.isAnnotationPresent(RegisterRender.class)) try {
+                RegisterRender details = field.getAnnotation(RegisterRender.class);
+                Block block = (Block) field.get(null);
+                if(block != null) {
+                    Item item = Item.getItemFromBlock(block);
+                    if(details.hasVariants())
+                        for(int meta = 0; meta < details.variants().length; meta++)
+                            ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(DomesticCraft.MODID, details.variants()[meta]), "inventory"));
+                    else
+                        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+                } else {
+                    DomesticCraft.LOGGER.error("Unable to register renders for block: " + field.getName() + "! The block cannot be null!");
+                }
+                registeredItems++;
+            } catch (Exception e) {
+                DomesticCraft.LOGGER.error("Unable to register renders for item block: " + field.getName() + "! The following error was thrown:");
+                DomesticCraft.LOGGER.catching(Level.ERROR, e);
+            }
+        }
+        DomesticCraft.LOGGER.info("Successfully registered renders for " + registeredItems + " items!");
     }
 
 }
